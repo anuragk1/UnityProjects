68ec0677247c5070ebd2b1e8bd6d885ba1b4fc29
anuragk1 <f20180879@gooa.bit-pilani.ac.in>
2021-07-08 11:52:07 +0530

    Added game - Dodge Block

---
+++ DodgeBlock/Assets/BlockSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+
+    public GameObject blockPrefab;
+
+    private float timeToSpawn = 2f;
+
+    public float timeBetweenWaves = 1f;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Time.time >= timeToSpawn)
+        {
+            SpawnBlocks();
+            timeToSpawn = Time.time + timeBetweenWaves;
+        }
+    }
+
+    void SpawnBlocks()
+    {
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if (i != randomIndex)
+            {
+                Instantiate(blockPrefab, spawnPoints[i].position, Quaternion.identity);
+            }
+        }
+    }
+}
