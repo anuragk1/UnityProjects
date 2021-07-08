5245b4dfaaff11a07c7ffdd8d20d2f8801351973
anuragk1 <f20180879@gooa.bit-pilani.ac.in>
2021-07-08 11:15:09 +0530

    Added ppo ml-agents custom env project

---
+++ rl/Assets/Scripts/BallAgentFollower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallAgentFollower : MonoBehaviour
+{
+    public Transform BallAgentTransform;
+
+    public Vector3 _cameraOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cameraOffset = transform.position - BallAgentTransform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = BallAgentTransform.position + _cameraOffset;
+    }
+}
